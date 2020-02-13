19bc3d0488cc23cfa41563e7d96e03172244d1d3
m҉4҉x҉X҉.҉!҉ <m4xx101101@gmail.com>
2020-02-13 16:36:00 +0530

    Update NTLM-findir.py

--- NTLM-findir.py
+++ NTLM-findir.py
@@ -1,3 +1,5 @@
+# pip install requests_ntlm
+
 import requests
 from requests_ntlm import HttpNtlmAuth
 from getpass import getpass
@@ -9,12 +11,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-usr','--username', dest = "username", help='AD username/Domain ID')
 parser.add_argument('-u','--url',dest = "url", help='Target URL')
 parser.add_argument('-w','--wordlist',dest = "wordlist", help='Path to Wordlist')
+parser.add_argument('-d','--domain',dest = "domain", help='Domain Name')
 args = parser.parse_args()
-geturl = args.url
 
 word_list = open(args.wordlist, 'r')
 threads = []
 password = getpass()
+
+def checkpassword() :
+	response = requests.get(args.url,auth=HttpNtlmAuth('args.domain\\args.username',password))
+	if response.status_code == 401:
+		print('\n')
+		print("401 - Unauthorized: Access is denied due to invalid credentials.")
+		exit()
+    	
 print(''' 
 ##    ## ######## ##       ##     ##         ######## #### ##    ## ########  #### ########  
 ###   ##    ##    ##       ###   ###         ##        ##  ###   ## ##     ##  ##  ##     ## 
@@ -22,20 +32,21 @@ print('''
 ## ## ##    ##    ##       ## ### ## ####### ######    ##  ## ## ## ##     ##  ##  ########  
 ##  ####    ##    ##       ##     ##         ##        ##  ##  #### ##     ##  ##  ##   ##   
 ##   ###    ##    ##       ##     ##         ##        ##  ##   ### ##     ##  ##  ##    ##  
-##    ##    ##    ######## ##     ##         ##       #### ##    ## ########  #### ##     ## v1.0 By m4xx!  ''')
+##    ##    ##    ######## ##     ##         ##       #### ##    ## ########  #### ##     ## v1.0  ''')
+
+checkpassword()
 
 try:
 	def scan(word):
 		url = args.url+word
 		session = requests.Session()
-		session.auth = HttpNtlmAuth('nseroot\\args.username',password)
-		#response = requests.get(url,auth=HttpNtlmAuth('nseroot\\hmt_aprasad',password))
+		session.auth = HttpNtlmAuth('args.domain\\args.username',password)
 		response = session.get(url)
 		if response.status_code == 200:
 			print(args.url)
 		else :
-		 	print(args.url+word,end="\r")
-	        
+			print(word,end="\r")
+	   	        
 	for word in word_list:
 		striped_word = word.strip()
 		t1 = threading.Thread(target=scan,args=(striped_word,))
